ad7088544a5a614ef58c3b4caacff733a80ffd91
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-03-20 01:32:26 +0100

    responsive wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-// import Header from './header/header';
 import Router from './router/router';
 import Footer from './footer/footer';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header/> */}
       <Router/>
       <Footer/>
     </div>
--- src/header/headerMain.js
+++ src/header/headerMain.js
@@ -9,11 +9,11 @@ function header() {
             <img src={Logo} alt='logo' className='header-left'/>
         </div>
         <nav>
-            <a href='#présentation' className='style-link'>Présentation</a>
-            <a href='#skill' className='style-link'>Compétences</a>
-            <a href='#portofolio' className='style-link'>Portofolio</a>
-            <a href='#about' className='style-link'>À propos</a>
-            <a href='#contact' className='style-link'>Contact</a>
+            <a href='#présentation' className='style-link-header'>Présentation</a>
+            <a href='#skill' className='style-link-header'>Compétences</a>
+            <a href='#portofolio' className='style-link-header'>Portofolio</a>
+            <a href='#about' className='style-link-header'>À propos</a>
+            <a href='#contact' className='style-link-header'>Contact</a>
         </nav>
     </header>
   );
